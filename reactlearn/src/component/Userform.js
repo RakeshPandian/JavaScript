9da1d62adc4b4d35735a59cd513f9938d23b1e3e
Rakesh Pandian <rakesh.pandian07@gmail.com>
2022-02-13 03:59:24 +0530

    Create and Update Completed

--- reactlearn/src/component/Userform.js
+++ reactlearn/src/component/Userform.js
@@ -4,14 +4,14 @@ import axios from "axios";
 class Userform extends Component{
     constructor(props){
     super(props);
-    this.state = {  
-      SelectedUser: null,  
+    this.state = {   
+      ID: "0",
       FirstName: "",
       LastName: "",
       Country: "",
       State: "",
       City: "",
-      Addres: "",
+      Address: "",
       Email: "",
       Phone: ""
     };
@@ -24,8 +24,23 @@ static defaultProps = {
    UserID: 0
 }
 
+updateUserData(user){
+  this.setState(st=> {return {
+    ID : user.id,
+    FirstName: user.firstName,
+    LastName : user.lastName,
+    Email : user.email,
+    Phone : user.phone,
+    Country: user.country,
+    State : user.state,
+    City : user.city,
+    Address : user.address
+  }; });
+}
+
 componentDidMount() {
     console.log("ComponentUsers");
+    if(this.props.UserID !="0"){
     var searchURL = `http://host.docker.internal:49153/api/Users/`+this.props.UserID;
     axios.get(searchURL,{
         headers: {
@@ -34,11 +49,10 @@ componentDidMount() {
       })
       .then(res => {
         const user= res.data;
-        this.setState(st=> {return {
-            SelectedUser: user
-        }; });
+        this.updateUserData(user);
         console.log(user);
       })
+    }
   }
 
 handleFormChange(evt){
@@ -49,54 +63,119 @@ handleFormChange(evt){
 
 handleSave(evt){
     evt.preventDefault();
-    var searchURL = `http://host.docker.internal:49153/api/Users/`+this.props.UserID;
-    axios.get(searchURL,{
+
+    const user = { 
+      firstName:this.state.FirstName,
+      lastName: this.state.LastName,
+      country: this.state.Country,
+      state: this.state.State,
+      city: this.state.City,
+      address: this.state.Address,
+      email: this.state.Email,
+      phone: this.state.Phone 
+    };
+if(this.props.UserID === "0"){
+    axios.post("http://localhost:49153/api/Users/",user,{
       headers: {
         'Content-Type': 'application/json'
      } 
     })
     .then(res => {
-      const userslist = res.data;
-    //   this.setState(st=> {return {persons: userslist}; 
-    //   });
-      console.log(userslist);
+      const saveresult = res.data;
+      this.updateUserData(saveresult);
+      console.log("saveresult", saveresult);
     })
   }
 
+else{
+  const user = { 
+    id: this.props.UserID,
+    firstName:this.state.FirstName,
+    lastName: this.state.LastName,
+    country: this.state.Country,
+    state: this.state.State,
+    city: this.state.City,
+    address: this.state.Address,
+    email: this.state.Email,
+    phone: this.state.Phone 
+  };
+  axios.put("http://localhost:49153/api/Users/"+this.props.UserID,user,{
+    headers: {
+        'Content-Type': 'application/json'
+   } 
+  })
+  .then(res => {
+    const saveresult = res.data;
+    this.updateUserData(saveresult);
+    console.log("saveresult", saveresult);
+  })
+}
+}
+
+
 render(){
     return(
-        <div className="container-fluid">
+        <div className="container-fluid m-2">
          <form onSubmit={this.handleSubmit}>
 
-          <div class="mb-3 row">
+          <div className="mb-2 row">
             <label className="col-sm-2 col-form-label" htmlFor='firstname'>First name</label>
             <div className="col-sm-10">
               <input type='text' className="form-control" id='firstname' name='FirstName' value={this.state.FirstName} 
                onChange={this.handleChange} />
             </div>
           </div>
-          <div class="mb-3 row">
+          <div className="mb-2 row">
             <label className="col-sm-2 col-form-label" htmlFor='lastname'>Last name</label>
             <div className="col-sm-10">
               <input type='text' className="form-control" id='lastname' name='LastName' value={this.state.LastName} 
                onChange={this.handleChange} />
             </div>
           </div>
-          <div class="mb-3 row">
+          <div className="mb-2 row">
             <label className="col-sm-2 col-form-label" htmlFor='email'>Email</label>
             <div className="col-sm-10">
               <input type='text' className="form-control" id='email' name='Email' value={this.state.Email} 
                onChange={this.handleChange} />
             </div>
           </div>
-          <div class="mb-3 row">
+          <div className="mb-2 row">
             <label className="col-sm-2 col-form-label" htmlFor='phone'>Phone</label>
             <div className="col-sm-10">
               <input type='text' className="form-control" id='phone' name='Phone' value={this.state.Phone} 
                onChange={this.handleChange} />
             </div>
           </div>
-          <button>Save</button>
+          <div className="mb-2 row">
+            <label className="col-sm-2 col-form-label" htmlFor='address'>Address</label>
+            <div className="col-sm-10">
+              <input type='text' className="form-control" id='address' name='Address' value={this.state.Address} 
+               onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="mb-2 row">
+            <label className="col-sm-2 col-form-label" htmlFor='country'>Country</label>
+            <div className="col-sm-10">
+              <input type='text' className="form-control" id='country' name='Country' value={this.state.Country} 
+               onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="mb-2 row">
+            <label className="col-sm-2 col-form-label" htmlFor='state'>State</label>
+            <div className="col-sm-10">
+              <input type='text' className="form-control" id='state' name='State' value={this.state.State} 
+               onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="mb-2 row">
+            <label className="col-sm-2 col-form-label" htmlFor='city'>City</label>
+            <div className="col-sm-10">
+              <input type='text' className="form-control" id='city' name='City' value={this.state.City} 
+               onChange={this.handleChange} />
+            </div>
+          </div>
+          <button className="btn btn-secondary m-2">Save</button>
+          <button className="btn btn-secondary m-2">Cancel</button>
         </form>
         </div>
     );
--- reactlearn/src/component/Users.js
+++ reactlearn/src/component/Users.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import Userform from "./Userform";
 
 class Users extends Component{
     constructor(props){
     super(props);
     this.state = {
       persons: [],
-      searchText: ""
+      searchText: "",
+      updateUserId: "0",
+      currentScreen: "USERS"
     };
     this.handleSearchSubmit = this.handleSearch.bind(this);
     this.handleSearchChange = this.handleSearchType.bind(this);
+    this.handleEditClick = this.handleEdit.bind(this);
+    this.handleDeleteClick = this.handleDelete.bind(this);
+    this.handleAddNewClick = this.handleAddNew.bind(this);
     console.log("This is Users");
 }
 
@@ -39,7 +45,7 @@ handleSearch(evt){
   var searchURL = `http://host.docker.internal:49153/api/Users/`+this.state.searchText;
   axios.get(searchURL,{
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
    } 
   })
   .then(res => {
@@ -50,45 +56,68 @@ handleSearch(evt){
   })
 }
 
+handleAddNew(){
+  this.setState(st=>{ return {
+    currentScreen: "EDIT",
+    updateUserId : "0"}
+  });
+}
+handleEdit(userID){
+  this.setState(st=>{ return {
+    currentScreen: "EDIT",
+    updateUserId : userID}
+  });
+}
+
+handleDelete(userID){
+  this.setState(st=>{ return {
+    currentScreen: "DELETE",
+    updateUserId : userID }
+  });
+}
 
 render(){
+  let screen;
+  if(this.state.currentScreen == "USERS"){
+    screen = (
+    <><div className='d-inline-flex flex-row'>
+        <form onSubmit={this.handleSearchSubmit} className='d-flex flex-row'>
+          <input className="form-control m-2" placeholder="Search by Name" id='search'
+            name='searchText' value={this.state.searchText} onChange={this.handleSearchChange} />
+          <button className="btn btn-secondary m-2">Search</button>
+        </form>
+        <button type="button" className="btn btn-secondary m-2" onClick={this.handleAddNewClick}>AddNew</button>
+      </div><div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Country</th>
+                <th scope="col">State</th>
+                <th scope="col">City</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.persons
+                .map(person => <tr key={person.id.toString()}>
+                  <td> {person.firstName + " " + person.lastName}  </td>
+                  <td> {person.country} </td>
+                  <td> {person.state} </td>
+                  <td> {person.city} </td>
+                  <td>  <button className="btn btn-secondary m-2" onClick={() =>this.handleEditClick(person.id.toString())}>Edit</button> </td>
+                  <td>  <button className="btn btn-secondary m-2" onClick={() =>this.handleDeleteClick(person.id.toString())}>Delete</button> </td>
+                </tr>
+                )}
+            </tbody>
+          </table>
+        </div></>
+    );
+  } else if(this.state.currentScreen == "EDIT"){
+    screen = <Userform UserID = {this.state.updateUserId}></Userform>
+  }
     return(
       <div className="container-fluid">
-      <div className='d-inline-flex flex-row'>
-      <form onSubmit={this.handleSearchSubmit} className='d-flex flex-row'>
-          <input  className="form-control m-2" placeholder="Search by Name"  id='search'
-          name='searchText'  value={this.state.searchText} onChange={this.handleSearchChange} />
-          <button className="btn btn-secondary m-2" >Search</button>
-      </form>
-      <button type="button" className="btn btn-secondary m-2">AddNew</button>
-      </div>
-        <div>
-      <table className="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Country</th>
-          <th scope="col">State</th>
-          <th scope="col">City</th>
-        </tr>
-      </thead>
-           <tbody>
-           {
-          this.state.persons
-            .map(person =>
-          <tr key={person.id.toString()}>
-          <td> {person.firstName + " " + person.lastName}  </td>
-          <td> {person.country} </td>
-          <td> {person.state} </td>
-          <td> {person.city} </td>
-          <td>  <button className="btn btn-secondary m-2" >Edit</button> </td>
-          <td>  <button className="btn btn-secondary m-2" >Delete</button> </td>
-          </tr>
-            )
-        }
-      </tbody>
-    </table>         
-    </div>
+      {screen}
     </div>
     );
 }
